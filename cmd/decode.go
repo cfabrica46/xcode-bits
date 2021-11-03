1b6bef0ae2521f0853f9c4f95ec1e906e8898cce
Cesar <c.caycho46@gmail.com>
2021-11-03 01:18:09 -0500

    problems

--- cmd/decode.go
+++ cmd/decode.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 
 	"github.com/spf13/cobra"
@@ -13,13 +15,28 @@ var decodeCmd = &cobra.Command{
 	Short: "Decode allows you to decode an integer and give you your data in strings",
 	Run: func(cmd *cobra.Command, args []string) {
 		var valueToDecode int
+		var valuePipe string
 		var err error
 
+		scanner := bufio.NewScanner(os.Stdin)
+
 		if len(args) == 0 {
-			fmt.Printf("Value: [ 1 - 4095 ]\n> ")
-			_, err = fmt.Scan(&valueToDecode)
-			if err != nil {
-				log.Fatal(err)
+			fmt.Println("a")
+			if scanner.Scan() {
+				fmt.Println("c")
+				valuePipe = scanner.Text()
+				valueToDecode, err = strconv.Atoi(valuePipe)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			fmt.Println("b")
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Value: [ 1 - 4095 ]\n> ")
+				_, err = fmt.Scan(&valueToDecode)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 		} else {
 			valueToDecode, err = strconv.Atoi(args[0])
